877649a41ebb0ce36c178c14a6a5d7eedf639b18
Adrien <adrien.ritter@epitech.eu>
2023-07-27 15:32:05 +0200

    [+]try to register with google and facebook

--- client/src/Components/Form/User/CreateUser.js
+++ client/src/Components/Form/User/CreateUser.js
@@ -1,78 +1,100 @@
 import React, { useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
+import { GoogleLogin } from 'react-oauth2'; // Replace with the correct OAuth2 library for Google authentication
+import FacebookLogin from 'react-facebook-login'; // Replace with the correct OAuth2 library for Facebook authentication
 
-export default function CreateUser ()
-{
+export default function CreateUser() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    admin: false,
+  });
 
-    const [form, setForm] = useState({
-        email : '',
-        password : '',
-        admin : false
-    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setForm({ ...form, [name] : value })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8000/createUser', form);
+      if (response.data === 'success') {
+        toast.success('Bienvenue !');
+      } else {
+        toast.error('Une erreur est survenue');
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      toast.error("Une erreur est survenue lors de l'ajout de l'article");
     }
+  };
 
-    const handleClick = async (e) => {
-        e.preventDefault();
+  const handleGoogleSuccess = (response) => {
+    console.log('Google response:', response);
+  };
 
-        // try 
-        // {
-        //     const response = await axios.post("http://localhost:8000/createUser", form);
-        //     if (response.data === "success") 
-        //     {
-        //         toast.success("Bienvenue !");
-        //     } 
-        //     else 
-        //     {
-        //         toast.error("Une erreur est survenue");
-        //     }
-        // } 
-        // catch (error) 
-        // {
-        //     console.error("Error submitting form:", error);
-        //     toast.error("Une erreur est survenue lors de l'ajout de l'article");
-        // }
+  const handleGoogleFailure = (error) => {
+    console.error('Google login error:', error);
+  };
 
-        console.log(form);
-    
-    }
+  const handleFacebookResponse = (response) => {
+    console.log('Facebook response:', response);
+  };
 
+  const handleFacebookFailure = (error) => {
+    console.error('Facebook login error:', error);
+  };
 
-    return (
+  return (
+    <div>
+      <h1 className="text-center my-5">Créer un compte</h1>
+      <div className="w-1/2 mx-auto">
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            required
+            placeholder="Email"
+            className="border"
+          />
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+            required
+            placeholder="Mot de passe"
+            className="border"
+          />
+          <button type="submit" className="border mt-5">
+            S'enregistrer
+          </button>
+        </form>
         <div>
-            <h1 className='text-center my-5'>Créer un compte</h1>
-            <div className='w-1/2 mx-auto'>
-                <form onSubmit={handleClick} className='flex flex-col'>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={form.email}
-                        onChange={handleChange}
-                        required
-                        placeholder="Email"
-                        className='border'
-                    />
-                    <label htmlFor="password">Mot de passe</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={form.password}
-                        onChange={handleChange}
-                        required
-                        placeholder="Mot de passe"
-                        className='border'
-                    />
-                    <button type="submit" className='border mt-5'>S'enregistrer</button>
-                </form>
-            </div>
+          <GoogleLogin
+            clientId="YOUR_GOOGLE_CLIENT_ID"
+            onSuccess={handleGoogleSuccess}
+            onFailure={handleGoogleFailure}
+          />
+          <FacebookLogin
+            appId="YOUR_FACEBOOK_APP_ID"
+            autoLoad={false}
+            fields="name,email,picture"
+            callback={handleFacebookResponse}
+            onFailure={handleFacebookFailure}
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
