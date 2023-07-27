4ea10cfc9387cc0036b018304c20ec3288f15edd
Quen2 <ZexFrost1@gmail.com>
2023-07-27 15:22:24 +0200

    [+] Form in order to create a new category is done

--- client/src/Components/Widgets/Navbar.js
+++ client/src/Components/Widgets/Navbar.js
@@ -7,9 +7,9 @@ export default function Navbar ()
     return (
         <div className="flex bg-slate-400 p-2 justify-evenly gap-8">
             <p>Chien</p>
-            <p>Druide</p>
             <p>Chat</p>
             <Link to='/createArticle'>Ajouter un article</Link>
+            <Link to='/createCategory'>Créer une catégorie</Link>
             <Link to='/login'>Se connecter</Link>
             <Link to='/register'>Créer un compte</Link>
             <Link to='/profil'>Profil</Link>
