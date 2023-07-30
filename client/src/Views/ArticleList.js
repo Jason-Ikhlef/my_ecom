620d9b5e06890cbda871ef1a128a5014146530d3
Jason-Ikhlef <jason.ikhlef@gmail.com>
2023-07-30 12:08:49 +0200

    [~] fix JSX in ArticleList after merge

--- client/src/Views/ArticleList.js
+++ client/src/Views/ArticleList.js
@@ -42,38 +42,40 @@ export default function SimpleSlider() {
       })
   }
 
-    return (
-      <div className="scroll">
-        <div className="wrap">
-          <div className="search border w-1/3 mx-auto my-5 flex justify-between relative ">
-              <input type="text" onChange={ (e) => setSearchValue(e.target.value) } className="searchTerm w-full p-2" placeholder="Que cherchez vous ?"></input>
-              <button type="submit" onClick={searchArticle} className="searchButton absolute right-0 mr-2 p-2">
-                <img src={Search} alt="search icon" className="w-[20px] h-[20px]"></img>
-              </button>
+  return (
+    <div className="scroll">
+      <div className="wrap">
+        <div className="search border w-1/3 mx-auto my-5 flex justify-between relative">
+          <input
+            type="text"
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="searchTerm w-full p-2"
+            placeholder="Que cherchez vous ?" />
+          <button type="submit" onClick={searchArticle} className="searchButton absolute right-0 mr-2 p-2">
+            <img src={Search} alt="search icon" className="w-[20px] h-[20px]" />
+          </button>
+        </div>
+    </div><button>Name</button><button>Category</button>
+    {articles.map((article) => (
+      <div key={article._id} className="bg-red-200 w-3/4 mx-auto mt-10 content_border">
+        <Link className="w-3/4 mx-auto" to={`/articles/${article._id}`} state={{ id: article._id }}>
+          <p className="text-center text-white mb-6 p-2 bg-[#4FBEB7]">{article.title}</p>
+          <img src={`http://localhost:8000/storage/${article.pictures[0]}`} className="w-[200px] mx-auto" alt="article img" />
+          <p className="text-center my-10">{article.description}</p>
+          <div className="flex justify-around pb-5">
+            {/* pour etat : article.state */}
+            <p>Stock: {article.stock}</p>
+            <p>{article.price} €</p>
+            <p>{article.caracteristics}</p>
+            {/* <p>{article.animalsName}</p>
+            <p>{article.categoriesName}</p>
+            <p>{article.subCategoriesName}</p> */}
           </div>
-        </div>
+        </Link>
       </div>
-      <button>Name</button>
-      <button>Category</button>
-      {articles.map((article) => (
-        <div key={article._id} className="bg-red-200 w-3/4 mx-auto mt-10 content_border">
-          <Link className="w-3/4 mx-auto" to={`/articles/${article._id}`} state={{ id : article._id}}>
-            <p className="text-center text-white mb-6 p-2 bg-[#4FBEB7]">{article.title}</p>
-            <img src={`http://localhost:8000/storage/${article.pictures[0]}`} className="w-[200px] mx-auto" alt="article img"></img> 
-            <p className="text-center my-10">{article.description}</p>
-            <div className="flex justify-around pb-5">
-              {/* pour etat : article.state */}
-              <p>Stock: {article.stock}</p>
-              <p>{article.price} €</p>
-              <p>{article.caracteristics}</p>
-              {/* <p>{article.animalsName}</p>
-              <p>{article.categoriesName}</p>
-              <p>{article.subCategoriesName}</p> */}
-            </div>
-          </Link>
-        ))}
-        </div>
-      </div>
-    );
+    ))}
+    </div>
+  );
+  
   }
   
