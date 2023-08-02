c932f3d7e5957f806813808b31a50d5cce6881a5
Quen2 <ZexFrost1@gmail.com>
2023-08-02 12:50:51 +0200

    [~] Fix hover for the navbar categories

--- client/src/Components/Form/FormArticle/UpdateArticle.js
+++ client/src/Components/Form/FormArticle/UpdateArticle.js
@@ -43,7 +43,7 @@ export default function UpdateArticle({ idArticle }) {
         };
 
         fetchArticle();
-    }, [id]);
+    }, [idArticle]);
 
     useEffect(() => {
         const fetchAnimals = async () => {
