098c425c81e0d70d3cd9b76256af76a25317b52b
Jason-Ikhlef <jason.ikhlef@gmail.com>
2023-08-07 15:42:46 +0200

    [~] remove images from storage when an article is deleted

--- server/routes/articles/delete.js
+++ server/routes/articles/delete.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs')
 
 const { articleCollection } = require("../../mongo");
 
 router.delete("/DeleteArticle/:id", async (req, res) => {
 
     const { id } = req.params
+    const uploadDir = path.join(__dirname, '../../storage');
 
     try {
+        const article = await articleCollection.findOne({_id: id});
+
+        article.pictures.forEach(element => {
+            try {
+                fs.unlinkSync(uploadDir + '/' + element)
+              } catch(err) {
+                console.error(err)
+              }
+        });
+
         await articleCollection.deleteOne({_id: id});
         res.json("success");
     } catch (e) {
