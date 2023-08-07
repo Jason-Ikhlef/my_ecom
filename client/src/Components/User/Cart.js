3b5d388598f914d07bafbdeca2a799a0d73234ba
Adrien <adrien.ritter@epitech.eu>
2023-08-07 15:10:02 +0200

    [~]fix merge

--- client/src/Components/User/Cart.js
+++ client/src/Components/User/Cart.js
@@ -6,7 +6,6 @@ import Loader from "../../Components/Widgets/Loader";
 import bin from "../../assets/delete-bin-line.svg";
 import mastercard from "../../assets/mastercard-6.svg";
 import paypal from "../../assets/paypal-3.svg";
-import visa from "../../assets/visa-4.svg";
 import { Link } from "react-router-dom";
 import Shipping from "../Widgets/Shipping";
 import { ToastContainer, toast } from "react-toastify";
@@ -144,265 +143,155 @@ const Cart = () => {
   if (userLoading || !easyPost || shippingLoading) {
     return <Loader />;
   }
-  
-  return (
-    <>
-    <ToastContainer />
-      {cart && (
-        <div className="w-3/4 mx-auto flex justify-center gap-4 mt-4">
-          {logIn === false ? (
-                        <div className="w-1/2 border flex flex-col justify-evenly items-center">
-                            <p className="my-4 text-2xl">
-                                Souhaitez-vous vous connecter avant de payer ?
-                            </p>
-                            <div className="flex flex-col justify-evenly items-center h-1/2">
-                                <button
-                                    onClick={() => {
-                                        window.location.href =
-                                            "http://localhost:3000/register";
-                                    }}
-                                    className="w-full text-center bg-[#4FBEB7] p-2 text-lg">
-                                    M'inscrire
-                                </button>
-                                <button
-                                    onClick={() => {
-                                        window.location.href =
-                                            "http://localhost:3000/login";
-                                    }}
-                                    className="w-full text-center bg-[#4FBEB7] p-2 text-lg">
-                                    Me connecter
-                                </button>
-                                <button
-                                    onClick={orderWithoutUser}
-                                    className="w-full text-center bg-[#4FBEB7] p-2 text-lg">
-                                    Continuer sans me connecter
-                                </button>
-                            </div>
-                        </div>
-                    ) : (
-          <div className="w-1/2 border">
-            <p className="p-4 text-2xl">Mon panier</p>
-            {cart.map((item, index) => (
-              <div className="bg-white p-2 border-b">
-                <div key={index} className="flex justify-evenly">
-                  <img
-                    src={`http://localhost:8000/storage/${item.img}`}
-                    alt="img articles"
-                    className="w-[100px] h-[100px]"
-                  ></img>
-                  <div className="flex flex-col w-1/3">
-                    <p>{item.price} €</p>
-                    <p className="whitespace-normal text-[12px]">{item.name}</p>
-                    <p>Qté : {item.quantity}</p>
-                    <img
-                      onClick={() =>
-                        deleteArticle(item.articleId, item.price, item.quantity)
-                      }
-                      src={bin}
-                      className="w-[20px] h-[20px] self-end z-10 cursor-pointer"
-                      alt="delete bin"
-                    ></img>
-                  </div>
-                </div>
-              </div>
-            ))}
+  return(
+<>
+  <ToastContainer />
+  {cart && (
+    <div className="w-3/4 mx-auto flex justify-center gap-4 mt-4">
+      {logIn === false ? (
+        <div className="w-1/2 border flex flex-col justify-evenly items-center">
+          <p className="my-4 text-2xl">
+            Souhaitez-vous vous connecter avant de payer ?
+          </p>
+          <div className="flex flex-col justify-evenly items-center h-1/2">
+            <button
+              onClick={() => {
+                window.location.href = "http://localhost:3000/register";
+              }}
+              className="w-full text-center bg-[#4FBEB7] p-2 text-lg"
+            >
+              M'inscrire
+            </button>
+            <button
+              onClick={() => {
+                window.location.href = "http://localhost:3000/login";
+              }}
+              className="w-full text-center bg-[#4FBEB7] p-2 text-lg"
+            >
+              Me connecter
+            </button>
+            <button
+              onClick={orderWithoutUser}
+              className="w-full text-center bg-[#4FBEB7] p-2 text-lg"
+            >
+              Continuer sans me connecter
+            </button>
           </div>
-          )}
-          <div className="w-1/4 flex flex-col h-fit gap-4 sticky top-0">
-            <div className="flex flex-col gap-4 border">
-              <p className="p-4 text-2xl">Total</p>
-              <hr></hr>
-              <div className="flex justify-between mx-4">
-                <p className="text-2xl">Livraison</p>
-                <p className="p-2">A venir</p>
-              </div>
-              <div className="flex justify-between mx-4">
-                <p className="text-2xl">Sous-total</p>
-                <p className="p-2">{totalPrice.toFixed(2)} €</p>
-              </div>
-              <div className="flex justify-between mx-4">
-                <p className="text-2xl">avec prime</p>
-                <p className="p-2">{totalPrice.toFixed(2)-5} €</p>
-                </div>
-              <div id="subbutton" className="w-full p-2 bg-[#4FBEB7]">
-              <Link to="http://localhost:3000/SubPage"><div>DEFILE STP</div></Link> 
-                </div>
-                <p className="text-2xl">Frais de port</p>
-                <p className="p-2">{
-                  totallySprice
-                } €</p>
-              </div>
-              <div className="flex justify-between mx-4">
-                <p className="text-2xl">Panier</p>
-                <p className="p-2">{totalPrice.toFixed(2)} €</p>
-              </div>
-              <div className="flex justify-between mx-4">
-                <p className="text-2xl">Sous-total</p>
-                <p className="p-2">{(Number(totallySprice) + Number(totalPrice)).toFixed(2)} €</p>
-              </div>
-              <div className="flex justify-between mx-4">
-                <p className="text-lg">Livraison</p>
-                <p className="p-2 text-sm">{selectedOffer.delivery_days} jours</p>
-              </div>
-              {easyPost && (
-                <div className="border w-full">
-                  <select className="w-full" onChange={(e) => selectOffer(e.target.value)}>
-                  {
-                    easyPost.map(item =>
-                      <option className="border" key={item.id} >{item.service}</option>   
-                    )
-                  }
-                  </select>
-                </div>
-              <button
-                onClick={newOrder}
-                className="bg-[#4FBEB7] w-3/4 p-2 mx-auto "
-              >
-                Paiement
-              </button>
-              <p className="px-4 text-2xl">Nous acceptons :</p>
-              <div className="flex justify-evenly mb-4">
+        </div>
+      ) : (
+        <div className="w-1/2 border">
+          <p className="p-4 text-2xl">Mon panier</p>
+          {cart.map((item, index) => (
+            <div className="bg-white p-2 border-b" key={index}>
+              <div className="flex justify-evenly">
                 <img
-                  src={visa}
-                  alt="icon visa"
-                  className="w-[50px] h-[50px]"
-                ></img>
-                <img
-                  src={mastercard}
-                  alt="icon visa"
-                  className="w-[50px] h-[50px]"
-                ></img>
-                <img
-                  src={paypal}
-                  alt="icon visa"
-                  className="w-[50px] h-[50px]"
-                ></img>
+                  src={`http://localhost:8000/storage/${item.img}`}
+                  alt="img articles"
+                  className="w-[100px] h-[100px]"
+                />
+                <div className="flex flex-col w-1/3">
+                  <p>{item.price} €</p>
+                  <p className="whitespace-normal text-[12px]">{item.name}</p>
+                  <p>Qté : {item.quantity}</p>
+                  <img
+                    onClick={() =>
+                      deleteArticle(item.articleId, item.price, item.quantity)
+                    }
+                    src={bin}
+                    className="w-[20px] h-[20px] self-end z-10 cursor-pointer"
+                    alt="delete bin"
+                  />
+                </div>
               </div>
             </div>
-            <div className="border w-full mx-auto p-4">
-              <button onClick={clearCart} className="w-full p-2 bg-[#4FBEB7]">
-                Vider le panier
-              </button>
-            </div>
+          ))}
+        </div>
+      )}
+
+      <div className="w-1/4 flex flex-col h-fit gap-4 sticky top-0">
+        <div className="flex flex-col gap-4 border">
+          <p className="p-4 text-2xl">Total</p>
+          <hr/>
+          <div className="flex justify-between mx-4">
+            <p className="text-2xl">Frais de port</p>
+            <p className="p-2">{totallySprice} €</p>
+          </div>
+        </div>
+        <div className="flex justify-between mx-4">
+          <p className="text-2xl">Panier</p>
+          <p className="p-2">{totalPrice.toFixed(2)} €</p>
+        </div>
+        <div className="flex justify-between mx-4">
+          <p className="text-2xl">Sous-total</p>
+          <p className="p-2">
+            {(Number(totallySprice) + Number(totalPrice)).toFixed(2)} €
+          </p>
+        </div>
+          <div className="flex justify-between mx-4">
+            <p className="text-2xl">avec prime</p>
+            <p className="p-2">{(totalPrice).toFixed(2)} €</p>
+          </div>
+        <div className="flex justify-between mx-4">
+          <p className="text-lg">Livraison</p>
+          <p className="p-2 text-sm">{selectedOffer.delivery_days} jours</p>
+        </div>
+          <div id="subbutton" className="w-full p-2 bg-[#4FBEB7]">
+            <Link to="http://localhost:3000/SubPage">DEFILE STP</Link>
+          </div>
+      </div>
+      {easyPost && (
+        <div>
+          <div className="border w-full">
+            <select
+              className="w-full"
+              onChange={(e) => selectOffer(e.target.value)}
+            >
+              {easyPost.map(item => (
+                <option className="border" key={item.id}>
+                  {item.service}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            onClick={newOrder}
+            className="bg-[#4FBEB7] w-3/4 p-2 mx-auto"
+          >
+            Paiement
+          </button>
+          <p className="px-4 text-2xl">Nous acceptons :</p>
+          <div className="flex justify-evenly mb-4">
+            <img
+              src={visa}
+              alt="icon visa"
+              className="w-[50px] h-[50px]"
+            />
+            <img
+              src={mastercard}
+              alt="icon visa"
+              className="w-[50px] h-[50px]"
+            />
+            <img
+              src={paypal}
+              alt="icon visa"
+              className="w-[50px] h-[50px]"
+            />
+          </div>
+
+          <div className="border w-full mx-auto p-4">
+            <button
+              onClick={clearCart}
+              className="w-full p-2 bg-[#4FBEB7]"
+            >
+              Vider le panier
+            </button>
           </div>
         </div>
       )}
-    </>
-  );
+    </div>
+  )}
+</>
+)};
 
-//     return (
-//         <>
-//             {cart && (
-//                 <div className="w-3/4 mx-auto flex justify-center gap-4 mt-4">
-//                     {logIn === false ? (
-//                         <div className="w-1/2 border flex flex-col justify-evenly items-center">
-//                             <p className="my-4 text-2xl">
-//                                 Souhaitez-vous vous connecter avant de payer ?
-//                             </p>
-//                             <div className="flex flex-col justify-evenly items-center h-1/2">
-//                                 <button
-//                                     onClick={() => {
-//                                         window.location.href =
-//                                             "http://localhost:3000/register";
-//                                     }}
-//                                     className="w-full text-center bg-[#4FBEB7] p-2 text-lg">
-//                                     M'inscrire
-//                                 </button>
-//                                 <button
-//                                     onClick={() => {
-//                                         window.location.href =
-//                                             "http://localhost:3000/login";
-//                                     }}
-//                                     className="w-full text-center bg-[#4FBEB7] p-2 text-lg">
-//                                     Me connecter
-//                                 </button>
-//                                 <button
-//                                     onClick={orderWithoutUser}
-//                                     className="w-full text-center bg-[#4FBEB7] p-2 text-lg">
-//                                     Continuer sans me connecter
-//                                 </button>
-//                             </div>
-//                         </div>
-//                     ) : (
-//                         <div className="w-1/2 border">
-//                             <p className="p-4 text-2xl">Mon panier</p>
-//                             {cart.map((item, index) => (
-//                                 <div className="bg-white p-2 border-b">
-//                                     <div
-//                                         key={index}
-//                                         className="flex justify-evenly">
-//                                         <img
-//                                             src={`http://localhost:8000/storage/${item.img}`}
-//                                             alt="img articles"
-//                                             className="w-[100px] h-[100px]"></img>
-//                                         <div className="flex flex-col w-1/3">
-//                                             <p>{item.price} €</p>
-//                                             <p className="whitespace-normal text-[12px]">
-//                                                 {item.name}
-//                                             </p>
-//                                             <p>Qté : {item.quantity}</p>
-//                                             <img
-//                                                 onClick={() =>
-//                                                     deleteArticle(
-//                                                         item.articleId,
-//                                                         item.price,
-//                                                         item.quantity
-//                                                     )
-//                                                 }
-//                                                 src={bin}
-//                                                 className="w-[20px] h-[20px] self-end z-10 cursor-pointer"
-//                                                 alt="delete bin"></img>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             ))}
-//                         </div>
-//                     )}
-//                     <div className="w-1/4 flex flex-col h-fit gap-4 sticky top-0">
-//                         <div className="flex flex-col gap-4 border">
-//                             <p className="p-4 text-2xl">Total</p>
-//                             <hr></hr>
-//                             <div className="flex justify-between mx-4">
-//                                 <p className="text-2xl">Livraison</p>
-//                                 <p className="p-2">A venir</p>
-//                             </div>
-//                             <div className="flex justify-between mx-4">
-//                                 <p className="text-2xl">Sous-total</p>
-//                                 <p className="p-2">{totalPrice.toFixed(2)} €</p>
-//                             </div>
-//                             <button
-//                                 onClick={newOrder}
-//                                 className="bg-[#4FBEB7] w-3/4 p-2 mx-auto">
-//                                 Paiement
-//                             </button>
-//                             <p className="px-4 text-2xl">Nous acceptons :</p>
-//                             <div className="flex justify-evenly mb-4">
-//                                 <img
-//                                     src={visa}
-//                                     alt="icon visa"
-//                                     className="w-[50px] h-[50px]"></img>
-//                                 <img
-//                                     src={mastercard}
-//                                     alt="icon visa"
-//                                     className="w-[50px] h-[50px]"></img>
-//                                 <img
-//                                     src={paypal}
-//                                     alt="icon visa"
-//                                     className="w-[50px] h-[50px]"></img>
-//                             </div>
-//                         </div>
-//                         <div className="border w-full mx-auto p-4">
-//                             <button
-//                                 onClick={clearCart}
-//                                 className="w-full p-2 bg-[#4FBEB7]">
-//                                 Vider le panier
-//                             </button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             )}
-//         </>
-//     );
-};
 
 export default Cart;
--- client/src/Components/Widgets/Footer.js
+++ client/src/Components/Widgets/Footer.js
@@ -2,47 +2,33 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 export default function Footer() {
-    let location = useLocation();
+  const location = useLocation();
+  const isArticlePage =
+    location.pathname === "/articles" ||
+    location.pathname.includes("/articles/") ||
+    location.pathname === "/admin/articles" ||
+    location.pathname === "/SubPage" ||
+    location.pathname === "/cart";
+  const isAdminArticlePage = location.pathname === "/admin/articles";
 
-    return (
-        <>
-            {location.pathname === "/articles" ||
-            window.location.pathname.includes("/articles/") ||
-            location.pathname === "/admin/articles" ||
-            location.pathname === "/SubPage" ||
-            location.pathname === "/cart" ? (
-            location.pathname === "/admin/articles" ? (
-
-                <div className="bg-[#4FBEB7] p-4 flex mt-5 w-full justify-evenly static bottom-0">
-                    <div>
-                        <p>Conditions générales d'utilisation</p>
-                    </div>
-                    <div>
-                        <p>©2023 CopainFront</p>
-                    </div>
-                    <div>
-                        <p>Mentions légales</p>
-                    </div>
-                    <div>
-                        <p>Nous contactez</p>
-                    </div>
-                </div>
-            ) : (
-                <div className="bg-[#4FBEB7] p-4 flex mt-5 w-full justify-evenly fixed bottom-0">
-                    <div>
-                        <p>Conditions générales d'utilisation</p>
-                    </div>
-                    <div>
-                        <p>©2023 CopainFront</p>
-                    </div>
-                    <div>
-                        <p>Mentions légales</p>
-                    </div>
-                    <div>
-                        <p>Nous contactez</p>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+  return (
+    <div
+      className={`bg-[#4FBEB7] p-4 flex mt-5 w-full justify-evenly ${
+        isAdminArticlePage ? "static bottom-0" : "fixed bottom-0"
+      }`}
+    >
+      <div>
+        <p>Conditions générales d'utilisation</p>
+      </div>
+      <div>
+        <p>©2023 CopainFront</p>
+      </div>
+      <div>
+        <p>Mentions légales</p>
+      </div>
+      <div>
+        <p>Nous contacter</p>
+      </div>
+    </div>
+  );
 }
