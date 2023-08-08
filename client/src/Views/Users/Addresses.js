86fdee6388ce5c959adf32879815cb8c1af5a65e
Quen2 <ZexFrost1@gmail.com>
2023-08-08 12:39:39 +0200

    [+] form for create a new adress is done

--- client/src/Views/Users/Addresses.js
+++ client/src/Views/Users/Addresses.js
@@ -8,6 +8,18 @@ export default function Addresses() {
    const { currentUser, userLoading } = User()
    const [isAddindAddress, setIsAddindAddress] = useState(null)
 
+//    const [form, setForm] = useState({
+//         country: '',
+//         city: '',
+//         zipcode : Number(),
+//         adress : ''
+//     });
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setForm({ ...form, [name]: value });
+//     };
+
    if (userLoading) {
 
         return <Loader />
@@ -18,14 +30,61 @@ export default function Addresses() {
     currentUser && (
         isAddindAddress ? (
             <>
-                <p>Adresse</p>
-                <p>Ville</p>
-                <p>ZIP</p>
-                <p>Pays</p>
-                <p>submit</p>
+                <p onClick={() => {setIsAddindAddress(null)}}>fromage</p>
+                <form /* onSubmit={handleSubmit} */ className="flex flex-col w-2/4 mx-auto mt-8 border rounded-xl p-2">
+                    <label htmlFor="country">Votre pays</label>
+                    <input
+                        type="text"
+                        id="country"
+                        name="country"
+                        /* value= {form.country} */
+                        /* onChange={handleChange} */
+                        required
+                        placeholder="Pays"
+                        className="border"
+                    />
+                    <label htmlFor="city">Votre ville</label>
+                    <input
+                        type="text"
+                        id="city"
+                        name="city"
+                        /* value= {form.city} */
+                        /* onChange={handleChange} */
+                        required
+                        placeholder="Ville"
+                        className="border"
+                    />
+                    <label htmlFor="city">Code postal</label>
+                    <input
+                        type="text"
+                        id="zipcode"
+                        name="zipcode"
+                        /* value= {form.zipcode} */
+                        /* onChange={handleChange} */
+                        required
+                        placeholder="Code postal"
+                        className="border"
+                    />
+                    <label htmlFor="adress">Votre adresse</label>
+                    <input
+                        type="text"
+                        id="adress"
+                        name="adress"
+                        /* value= {form.adress} */
+                        /* onChange={handleChange} */
+                        required
+                        placeholder="Adresse"
+                        className="border"
+                    />
+                    <button type="submit" className="mt-5 bg-[#4FBEB7] p-2 mb-2 rounded-lg">
+                        Créer la nouvelle adresse
+                    </button>
+                </form>
             </>
         ) : (
-            <p>les adresses</p>
+            <div>
+                <p onClick={() => {setIsAddindAddress(true)}}>fromage</p>
+            </div>
         )
     )
    )
