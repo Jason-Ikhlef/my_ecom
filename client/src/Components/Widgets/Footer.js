f426004788a51689601da1360ea68d40668e1e02
Dubois Guillaume <guillaume.dubois@epitech.eu>
2023-08-10 04:27:01 -0400

    [~] fix Footer display

--- client/src/Components/Widgets/Footer.js
+++ client/src/Components/Widgets/Footer.js
@@ -2,33 +2,29 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 export default function Footer() {
-  const location = useLocation();
-  const isArticlePage =
-    location.pathname === "/articles" ||
-    location.pathname.includes("/articles/") ||
-    location.pathname === "/admin/articles" ||
-    location.pathname === "/SubPage" ||
-    location.pathname === "/cart";
-  const isAdminArticlePage = location.pathname === "/admin/articles";
+    const location = useLocation();
+    const isArticlePage =
+        location.pathname === "/articles" ||
+        location.pathname === "/admin/articles" ||
+        location.pathname === "/SubPage";
 
-  return (
-    <div
-      className={`bg-[#4FBEB7] p-4 flex mt-5 w-full justify-evenly ${
-        isAdminArticlePage ? "static bottom-0" : "fixed bottom-0"
-      }`}
-    >
-      <div>
-        <p>Conditions générales d'utilisation</p>
-      </div>
-      <div>
-        <p>©2023 CopainFront</p>
-      </div>
-      <div>
-        <p>Mentions légales</p>
-      </div>
-      <div>
-        <p>Nous contacter</p>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className={`bg-[#4FBEB7] p-4 flex mt-5 w-full justify-evenly ${
+                isArticlePage ? "relative" : "fixed bottom-0"
+            }`}>
+            <div>
+                <p>Conditions générales d'utilisation</p>
+            </div>
+            <div>
+                <p>©2023 CopainFront</p>
+            </div>
+            <div>
+                <p>Mentions légales</p>
+            </div>
+            <div>
+                <p>Nous contacter</p>
+            </div>
+        </div>
+    );
 }
