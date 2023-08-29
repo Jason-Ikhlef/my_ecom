00f82d07f44b80ee49ba02da579e2fa9fc56e97c
Jason-Ikhlef <jason.ikhlef@gmail.com>
2023-08-29 14:14:47 +0200

    [~] most ordered and most wishlisted added in article general data

--- server/routes/data/article_general.js
+++ server/routes/data/article_general.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
-const { articleCollection } = require("../../mongo");
+const {
+  articleCollection,
+  userCollection,
+  googleCollection,
+  facebookCollection,
+} = require("../../mongo");
+const { log } = require("console");
 
 router.get("/get_article_data", async (req, res) => {
   let date = new Date();
@@ -10,12 +16,78 @@ router.get("/get_article_data", async (req, res) => {
   const fileName = `general_article_${date}.csv`;
 
   function escapeCSVValue(value) {
-
-    if (value.includes(",") || value.includes('"') || value.includes("\n")) {
+    if (
+      typeof value === "string" &&
+      (value.includes(",") || value.includes('"') || value.includes("\n"))
+    ) {
       return `"${value.replace(/"/g, '""')}"`;
     } else {
       return value;
     }
+  }
+
+  function mostOrderedArticle(userList) {
+    const articlesUniqueOrdersCounter = {};
+    const articlesQuantityCounter = {};
+
+    userList.forEach((user) => {
+      const oldOrders = user.old_orders || [];
+      oldOrders.forEach((order) => {
+        const cart = order.cart || [];
+        cart.forEach((item) => {
+          const articleId = item.articleId;
+          const quantity = item.quantity;
+
+          if (articleId) {
+            articlesUniqueOrdersCounter[articleId] =
+              (articlesUniqueOrdersCounter[articleId] || 0) + 1;
+
+            articlesQuantityCounter[articleId] =
+              (articlesQuantityCounter[articleId] || 0) + quantity;
+          }
+        });
+      });
+    });
+
+    let mostOrderedByUnique = Object.entries(articlesUniqueOrdersCounter).sort(
+      (a, b) => b[1] - a[1]
+    )[0];
+
+    let mostOrderedByQuantity = Object.entries(articlesQuantityCounter).sort(
+      (a, b) => b[1] - a[1]
+    )[0];
+
+    return { mostOrderedByUnique, mostOrderedByQuantity };
+  }
+
+  function mostWishListedArticle(userList) {
+    const itemsUniqueCounter = {};
+    const itemsQuantityCounter = {};
+
+    userList.forEach((user) => {
+      const cart = user.cart || [];
+      cart.forEach((item) => {
+        const articleId = item.articleId;
+        const quantity = item.quantity;
+
+        if (articleId) {
+          itemsUniqueCounter[articleId] =
+            (itemsUniqueCounter[articleId] || 0) + 1;
+          itemsQuantityCounter[articleId] =
+            (itemsQuantityCounter[articleId] || 0) + quantity;
+        }
+      });
+    });
+
+    let mostOrderedByUnique = Object.entries(itemsUniqueCounter).sort(
+      (a, b) => b[1] - a[1]
+    )[0];
+
+    let mostOrderedByQuantity = Object.entries(itemsQuantityCounter).sort(
+      (a, b) => b[1] - a[1]
+    )[0];
+
+    return { mostOrderedByUnique, mostOrderedByQuantity };
   }
 
   function jsonToCsv(items) {
@@ -50,15 +122,132 @@ router.get("/get_article_data", async (req, res) => {
     return data;
   }
 
-  const obj = await articleCollection.find({});
+  async function getTopArticle(items) {
+    const { mostOrderedByUnique, mostOrderedByQuantity } =
+      mostOrderedArticle(items);
 
-  const data = jsonToCsv(obj);
+    const byUniqueArticle = await articleCollection.findOne({
+      _id: mostOrderedByUnique[0],
+    });
+    const byQuantityArticle = await articleCollection.findOne({
+      _id: mostOrderedByQuantity[0],
+    });
 
-  const header = Object.keys(data[0]).join(",");
-  let values = "";
-  let count = 0;
+    const firstRow = jsonToCsvArticle(byUniqueArticle, mostOrderedByUnique[1]);
 
-  data.forEach((element) => {
+    const secondRow = jsonToCsvArticle(
+      byQuantityArticle,
+      mostOrderedByQuantity[1]
+    );
+
+    return [firstRow, secondRow];
+  }
+
+  async function getTopCartArticle(items) {
+    const { mostOrderedByUnique, mostOrderedByQuantity } =
+      mostWishListedArticle(items);
+
+    const byUniqueArticle = await articleCollection.findOne({
+      _id: mostOrderedByUnique[0],
+    });
+    const byQuantityArticle = await articleCollection.findOne({
+      _id: mostOrderedByQuantity[0],
+    });
+
+    const firstRow = jsonToCsvCartArticle(byUniqueArticle, mostOrderedByUnique[1]);
+
+    const secondRow = jsonToCsvCartArticle(byQuantityArticle,mostOrderedByQuantity[1]);
+
+    return [firstRow, secondRow];
+  }
+
+  function jsonToCsvArticle(article, quantity) {
+    const row = {
+      Nom_article: article.title,
+      Description: article.description,
+      Caractéristiques: article.caracteristics,
+      Prix: article.price,
+      Propriété: article.property,
+      Nombre_images: article.pictures.length,
+      Animal: article.animalsName,
+      Categorie: article.categoriesName,
+      Sous_catégorie: article.subCategoriesName,
+      Stock: article.stock,
+      Poids: article.weight,
+      Reduction: article.reduction,
+      Nouveau:
+        article.isNewState === "new"
+          ? "Oui"
+          : article.isNewState === "forced"
+          ? "Oui"
+          : "Non",
+      Recommandé: article.recommanded ? "Oui" : "Non",
+      Quantité_commandée: quantity,
+    };
+
+    return row;
+  }
+
+  function jsonToCsvCartArticle(article, quantity) {
+    const row = {
+      Nom_article: article.title,
+      Description: article.description,
+      Caractéristiques: article.caracteristics,
+      Prix: article.price,
+      Propriété: article.property,
+      Nombre_images: article.pictures.length,
+      Animal: article.animalsName,
+      Categorie: article.categoriesName,
+      Sous_catégorie: article.subCategoriesName,
+      Stock: article.stock,
+      Poids: article.weight,
+      Reduction: article.reduction,
+      Nouveau:
+        article.isNewState === "new"
+          ? "Oui"
+          : article.isNewState === "forced"
+          ? "Oui"
+          : "Non",
+      Recommandé: article.recommanded ? "Oui" : "Non",
+      Quantité_panier: quantity,
+    };
+
+    return row;
+  }
+
+  const articles = await articleCollection.find({});
+
+  const obj = await userCollection.find({});
+  const obj2 = await googleCollection.find({});
+  const obj3 = await facebookCollection.find({});
+
+  function mergeSets(targetSet, sourceSet) {
+    for (const sourceObj of sourceSet) {
+      const matchingObjIndex = targetSet.findIndex(
+        (targetObj) => targetObj.email === sourceObj.email
+      );
+
+      if (matchingObjIndex !== -1) {
+        Object.assign(targetSet[matchingObjIndex], sourceObj);
+      } else {
+        targetSet.push(sourceObj);
+      }
+    }
+  }
+
+  mergeSets(obj, obj2);
+  mergeSets(obj, obj3);
+
+  const articlesInfos = jsonToCsv(articles);
+  const topArticle = await getTopArticle(obj);
+  const topCartArticle = await getTopCartArticle(obj);
+
+  const header = Object.keys(articlesInfos[0]).join(",");
+  const headerTopArticle = Object.keys(topArticle[0]).join(",");
+  const headerTopCartArticle = Object.keys(topCartArticle[0]).join(",");
+  let values, valuesTopArticleUnique = "";
+
+  articlesInfos.forEach((element) => {
     values +=
       escapeCSVValue(element.Nom_article) +
       "," +
@@ -68,7 +257,7 @@ router.get("/get_article_data", async (req, res) => {
       "," +
       element.Prix +
       "," +
-      escapeCSVValue(element.Propriété) + 
+      escapeCSVValue(element.Propriété) +
       "," +
       element.Nombre_images +
       "," +
@@ -88,10 +277,35 @@ router.get("/get_article_data", async (req, res) => {
       "," +
       element.Recommandé +
       "\n";
-    count++;
   });
 
-  const content = `${header}\n${values}`;
+  for (const property in topArticle[0]) {
+    valuesTopArticleUnique += escapeCSVValue(topArticle[0][property]) + ",";
+  }
+  valuesTopArticleUnique += "\n";
+
+  let valuesTopArticleQuantity = "";
+
+  for (const property in topArticle[1]) {
+    valuesTopArticleQuantity += escapeCSVValue(topArticle[1][property]) + ",";
+  }
+  valuesTopArticleQuantity += "\n";
+
+  let valuesTopCartArticleUnique = "";
+
+  for (const property in topCartArticle[0]) {
+    valuesTopCartArticleUnique += escapeCSVValue(topCartArticle[0][property]) + ",";
+  }
+  valuesTopCartArticleUnique += "\n";
+
+  let valuesTopCartArticleQuantity = "";
+
+  for (const property in topCartArticle[1]) {
+    valuesTopCartArticleQuantity += escapeCSVValue(topCartArticle[1][property]) + ",";
+  }
+  valuesTopCartArticleQuantity += "\n";
+
+  const content = `${header}\n${values}\n\nArticle unique le plus commandé\n${headerTopArticle}\n${valuesTopArticleUnique}\n\nArticle le plus commandé par quantité\n${headerTopArticle}\n${valuesTopArticleQuantity}\nArticle unique dans le plus de panier\n${headerTopCartArticle}\n${valuesTopCartArticleUnique}\n\nArticle dans le plus de panier par quantité\n${headerTopCartArticle}\n${valuesTopCartArticleQuantity}`;
 
   fs.writeFile(fileName, content, "utf-8", (err) => {
     if (err) {
