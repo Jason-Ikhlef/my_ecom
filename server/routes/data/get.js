0c23619b91f4cece17de0fec04f30c9d24c486e8
Jason-Ikhlef <jason.ikhlef@gmail.com>
2023-08-29 11:00:11 +0200

    [~] get.js display connexion type for users and remove csv-fast library

--- server/routes/data/get.js
+++ server/routes/data/get.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const path = require('path');
-const { format } = require("@fast-csv/format");
 
 const {
   userCollection,
@@ -15,7 +13,6 @@ router.get("/get_user_data", async (req, res) => {
   let date = new Date
   date = date.toLocaleDateString("fr").replaceAll("/", "-");
   const fileName = `general_user_${date}.csv`;
-  const csvFile = fs.createWriteStream(fileName);
 
   function averagePrice(old_orders) {
     let avgPrice = 0;
@@ -115,16 +112,17 @@ router.get("/get_user_data", async (req, res) => {
         Cartes_enregistrées: item.data.cards.length,
         Abonnement: subType(item.subscribed),
         Admin: item.admin ? "Admin" : "Client",
-        '-':'',
         Commandes: item.old_orders.length,
         Cmd_attentes: wtgCmd,
         Cmd_livrées: item.old_orders.length - wtgCmd,
         Objets_panier: item.cart.length,
-        '-':'',
         Prix_moyen_commande: averagePrice(item.old_orders),
         Argent_investi: totalPrice(item.old_orders),
         Nb_objet_moyen: averageObjets(item.old_orders),
-        Nb_objet_total: totalObjects(item.old_orders)
+        Nb_objet_total: totalObjects(item.old_orders),
+        Utilisateurs_classiques: totalUser,
+        Utilisateurs_google: totalGoogleUser,
+        Utilisateurs_facebook: totalFbUser,
       };
 
       csvRows.push(csvRow);
@@ -136,6 +134,14 @@ router.get("/get_user_data", async (req, res) => {
   const obj = await userCollection.find({});
   const obj2 = await googleCollection.find({});
   const obj3 = await facebookCollection.find({});
+
+  const totalUser = obj.length;
+  const totalGoogleUser = obj2.length;
+  const totalFbUser = obj3.length;
+
+  console.log(totalUser);
+  console.log(totalGoogleUser);
+  console.log(totalFbUser);
 
   function mergeSets(targetSet, sourceSet) {
     for (const sourceObj of sourceSet) {
@@ -154,57 +160,71 @@ router.get("/get_user_data", async (req, res) => {
   mergeSets(obj, obj2);
   mergeSets(obj, obj3);
 
-  const csvRows = jsonToCsv(obj);
+  const data = jsonToCsv(obj);
 
-  const customHeaders = [
-    "Email",
-    "Adresses_enregistrées",
-    "Cartes_enregistrées",
-    "Abonnement",
-    "Admin",
-    "",
-    "Commandes",
-    "Cmd_attentes",
-    "Cmd_livrées",
-    "Objets_panier",
-    "",
-    "Prix_moyen_commande",
-    "Argent_investi",
-    "Nb_objet_moyen",
-    "Nb_objet_total",
-  ];
+  const header = Object.keys(data[0]).join(",");
+  let values = "";
+  let count = 0;
 
-  const csvStream = format({ headers: customHeaders });
-  csvStream.pipe(csvFile);
+  data.forEach((element) => {
+      values +=
+        element.Email +
+        "," +
+        element.Adresses_enregistrées +
+        "," +
+        element.Cartes_enregistrées +
+        "," +
+        element.Abonnement +
+        "," +
+        element.Admin +
+        "," +
+        element.Commandes +
+        "," +
+        element.Cmd_attentes +
+        "," +
+        element.Cmd_livrées +
+        "," +
+        element.Objets_panier +
+        "," +
+        element.Prix_moyen_commande +
+        "," +
+        element.Argent_investi +
+        "," +
+        element.Nb_objet_moyen +
+        "," +
+        element.Nb_objet_total +
+        "," +
+        (count === 0 ? element.Utilisateurs_classiques : "") +
+        "," +
+        (count === 0 ? element.Utilisateurs_google : "") +
+        "," +
+        (count === 0 ? element.Utilisateurs_facebook : "") +
+        "\n";
 
-  for (const row of csvRows) {
-    csvStream.write(row);
-  }
+        count++;
+  });
+  
+  const content = `${header}\n${values}`;
 
-  csvStream.end();
-
-  csvFile.on("finish", () => {
-
-    const filePath = path.join(__dirname, `../../${fileName}`);
-    
-    res.status(200).download(filePath, fileName, (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Une erreur s'est produite lors du téléchargement.");
-      }
-
-      fs.unlink(filePath, (unlinkErr) => {
-        if (unlinkErr) {
-          console.error("Erreur lors de la suppression du fichier :", unlinkErr);
+  fs.writeFile(fileName, content, "utf-8", (err) =>  {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Une erreur s'est produuite lors de la création du fichier CSV.");
+    } else {
+      res.status(200).download(fileName, fileName, (downloadErr) => {
+        if (downloadErr) {
+          console.error(downloadErr);
+          res.status(500).send("Une erreur s'est produite lors du téléchargement.");
+        } else {
+          fs.unlink(fileName, (unlinkErr) => {
+            if (unlinkErr) {
+              console.error("Erreur lors de la suppression du fichier :", unlinkErr)
+            }
+          })
         }
-      });
-    });
-  });
-
-  csvFile.on("error", (err) => {
-    console.error(err);
-    res.status(500).send("Une erreur s'est produite lors de la création du fichier");
-  });
+      })
+    }
+  })
 });
 
 module.exports = router;
